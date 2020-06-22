0d89fbc7ce6ad03bf2c42b6b2ef1569155e12c02
Shubham Takankhar <takankhar@gmail.com>
2020-06-22 19:22:24 +0530

    few updates

--- ES6/insertPosition.js
+++ ES6/insertPosition.js
@@ -2,23 +2,40 @@
 
 var array =[2,3,5,6,3,2,2]
 
+//method 1
 
-function postioner(arr,ele){
+// function postioner(arr,ele){
 
-    var array = arr.sort()
+//     var array = arr.sort()
   
-    return  array.some(element => {
+//     return  array.some(element => {
 
-         if (element > ele){
-        console.log(element)
-        return element
+//          if (element > ele){
+//         console.log(element)
+//         return element
 
-        }
-        else{
-            NaN
-        }
-    })
+//         }
+//         else{
+//             NaN
+//         }
+//     })
+// }
+
+
+// console.log (postioner(array,4))
+
+
+//METHOD 2
+
+var num = 3
+
+function postioner(arr,num){
+    var i =0
+    arr.sort()
+    while (num > arr[i]){
+    i++
+    }
+    return i
 }
 
-
-console.log (postioner(array,4))
+console.log(postioner(array,num))
